0f8f28244809d0eba369932f71ff49219535d3aa
Youness El idrissi <youness.el-idrissi@etu.unice.fr>
2019-05-12 15:36:13 +0200

    computing a score every 5000 episodes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sarsa
 import qlearning
-
+import numpy as np
 
 
 def main():
-    epsilon = 0.5
+    epsilon = 0.1
     total_episodes = 100000
     max_steps = 100
 
-    lr_rate = 0.1
+    lr_rate = 0.01
     gamma = 0.96
 
     algos_f = {
@@ -16,8 +16,13 @@ def main():
         "2" : sarsa,
     }
 
-    # Here we chose an algo
-    Q = algos_f["1"].algo(epsilon, total_episodes, max_steps, lr_rate, gamma)
+    algos_f["1"].algo(epsilon, total_episodes, max_steps, lr_rate, gamma)
+    '''
+    for total_episodes in [1000,5000,10000,50000,100000]:
+        print('total episodes', total_episodes)
+        # Here we chose an algo
+        algos_f["1"].algo(epsilon, total_episodes, max_steps, lr_rate, gamma)
+        print()'''
 
 
 if __name__ == "__main__":
--- qlearning.py
+++ qlearning.py
@@ -3,7 +3,6 @@ from gym import wrappers, logger
 
 import numpy as np
 
-gym.logger.set_level(50)
 env = gym.make('FrozenLake-v0')
 
 min_epsilon = 0.1
@@ -55,6 +54,24 @@ def algo(epsilon, total_episodes, max_steps, lr_rate, gamma):
                 break
             epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
 
+        if episode % 5000 == 0:
+            # report every 5000 steps, test 100 games to get avarage point score for statistics and verify if it is solved
+            rew_average = 0.
+            for i in range(100):
+                obs = env.reset()
+                done = False
+                while done != True:
+                    action = np.argmax(Q[obs])
+                    obs, rew, done, info = env.step(action)  # take step using selected action
+                    rew_average += rew
+            rew_average = rew_average / 100
+            print('Episode {} avarage reward: {}'.format(episode, rew_average))
+
+            if rew_average > 0.8:
+                # FrozenLake-v0 defines "solving" as getting average reward of 0.78 over 100 consecutive trials.
+                # Test it on 0.8 so it is not a one-off lucky shot solving it
+                print("Frozen lake solved")
+                break
     score = rewards / total_episodes
     print('played',total_episodes)
     print('won',rewards)
